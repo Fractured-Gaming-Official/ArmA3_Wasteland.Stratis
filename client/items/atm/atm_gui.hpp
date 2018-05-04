bb2bc6954fa8dbd38acb7c3d63d9a2ebeeea3dce
George McClain <McClainG9616@gmail.com>
2018-05-04 04:31:35 -0400

    More ATM

--- client/items/atm/atm_gui.hpp
+++ client/items/atm/atm_gui.hpp
@@ -274,7 +274,7 @@ class AtmGUI
 	class AtmGreenButton : AtmButton
 	{
 		colorBackground[] = {A3W_UICOLOR_R, A3W_UICOLOR_G, A3W_UICOLOR_B, 0.8};
-		colorFocused[] = {0, 0.3, 0, 1}; // pulse
+		colorFocused[] = {0.3, 0, 0, 1}; // pulse
 		colorBackgroundActive[] = {A3W_UICOLOR_R, A3W_UICOLOR_G, A3W_UICOLOR_B, 1};
 	};
 
